57fc56142d44e58baf1cb0db39213377f2ba7d09
Dũng Nguyễn Đình <dungnguyendinh1052000@gmail.com>
2020-11-16 10:53:01 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QComboBox, QGridLayout
+from PyQt5 import uic, QtOpenGL
+import numpy as np
+import pyqtgraph as pg
+import random
+
+pg.setConfigOption('background', 'w')
+
+class main(QMainWindow):
+    def __init__(self):
+        super(main, self).__init__()
+        uic.loadUi('./form.ui', self)
+        self.option = self.findChild(QComboBox, 'comboBox')
+        self.option.addItems(["20 mA", u"200 µA", u"2 µA"])
+        self.option2 = self.findChild(QComboBox, 'comboBox_2')
+        self.option2.addItems(["Potential (V)", "Current (mA)", "DAC Code"])
+
+        self.dynamicPlt = pg.PlotWidget(self)
+
+        self.dynamicPlt.move(0,550)
+        self.dynamicPlt.resize(1440,200)
+
+        self.dynamicPlt2 = pg.PlotWidget(self)
+
+        self.dynamicPlt2.move(777,0)
+        self.dynamicPlt2.resize(663,519)
+
+        self.timer2 = pg.QtCore.QTimer()
+        self.timer2.timeout.connect(self.update)
+        self.timer2.start(200)
+
+    def update(self):
+        z = np.random.normal(size=1)
+        u = np.random.normal(size=1)
+        self.dynamicPlt.plot(z,u,pen=None, symbol='o')
+        self.dynamicPlt2.plot(z,u,pen=None, symbol='o')
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = main()
+    main_window.show()
+    sys.exit(app.exec_())
