8007c43b6d3d606b1f196947c18da99e872e7522
dungnd <dungnguyendinh1052000@gmail.com>
2020-12-04 01:40:28 +0700

    connect usb and show vid+pid

--- main.py
+++ main.py
@@ -9,12 +9,70 @@ import numpy as np
 import pyqtgraph as pg
 import random
 import copy
+import usb
 
 pg.setConfigOption('background', 'w')
 
 x = [450, 594, 310, 450, 594, 310, 450, 594]
 y = [3, 3, 115, 115, 115, 227, 227, 227]
-y_size = [0, 120, 240, 360, 480]
+y_size = [56, 176, 296, 416, 536]
+
+usb_vid = "0xa0a0"  # Default USB vendor ID, can also be adjusted in the GUI
+usb_pid = "0x0002"  # Default USB product ID, can also be adjusted in the GUI
+
+dev = None  # Global object which is reserved for the USB device
+
+
+class States:
+    """Expose a named list of states to be used as a simple state machine."""
+    NotConnected, Idle_Init, Idle, Measuring_Offset, Stationary_Graph, Measuring_CV, Measuring_CD, Measuring_Rate = range(
+        8)
+
+
+state = States.NotConnected  # Initial state
+
+
+def connect_disconnect_usb():
+    """Toggle the USB device between connected and disconnected states."""
+    global dev, state
+    if dev is not None:  # If the device is already connected, then this function should disconnect it
+        usb.util.dispose_resources(dev)
+        dev = None
+        state = States.NotConnected
+        main_window.usb_connect.setText("Connect")
+        # log_message("USB Interface disconnected.")
+        return
+
+    # Otherwise, try to connect
+    usb_vid_string = str(main_window.text_vid.text())
+    usb_pid_string = str(main_window.text_pid.text())
+    dev = usb.core.find(idVendor=int(usb_vid_string, 0),
+                        idProduct=int(usb_pid_string, 0))
+    print(dev)
+    if dev is None:
+        print("USB Device Not Found, No USB device was found with VID %s and PID %s. Verify the vendor/product ID and check the USB connection." %
+              (usb_vid_string, usb_pid_string))
+    else:
+        main_window.usb_connect.setText("Disconnect")
+        # log_message("USB Interface connected.")
+        try:
+            print(dev.manufacturer, dev.product, dev.serial_number)
+            main_window.label_manufacture.setText(
+                "Manufacture:   %s" % (dev.manufacturer))
+            main_window.label_product.setText("Product:   %s" % (dev.product))
+            main_window.label_serial.setText(
+                "Serial #:   %s" % (dev.serial_number))
+
+            # hardware_device_info_text.setText("Manufacturer: %s\nProduct: %s\nSerial #: %s" % (
+            #     dev.manufacturer, dev.product, dev.serial_number))
+            # get_calibration()
+            # set_cell_status(False)  # Cell off
+            # set_control_mode(False)  # Potentiostatic control
+            # set_current_range()  # Read current range from GUI
+            state = States.Idle_Init  # Start idle mode
+        except ValueError:
+            print("### Error USB")
+            pass  # In case the device is not yet calibrated
 
 
 class Frame(QPushButton):
@@ -32,8 +90,8 @@ class Frame(QPushButton):
         if (main_window.frame_y.pos().y()-50 < self.pos().y() < main_window.frame_y.pos().y()+50):
             self.check_move = 0
             self.resize(120, 73)
-            self.setStyleSheet(
-                "background-color: rgb(136, 138, 133)")
+            # self.setStyleSheet(
+            #     "background-color: #202932;")
             self.move(y_size[main_window.status_line],
                       main_window.frame_y.pos().y())
             main_window.status_line += 1
@@ -45,7 +103,7 @@ class Frame(QPushButton):
             if self.index_line:
                 main_window.status_line -= 1
                 self.index_line = 0
-            self.setStyleSheet("background-color: #181818;")
+            # self.setStyleSheet("background-color: #181818;")
             self.move(x[self.index_table], y[self.index_table])
 
 
@@ -108,13 +166,26 @@ class main(QMainWindow):
         self.main_widget = self.findChild(QWidget, 'main_widget')
         self.option = self.findChild(QComboBox, 'comboBox')
 
+        self.usb_connect = self.findChild(QPushButton, 'usb_connect')
+        self.usb_connect.clicked.connect(connect_disconnect_usb)
+
         self.option.addItems(["20 mA", u"200 µA", u"2 µA"])
         self.option2 = self.findChild(QComboBox, 'comboBox_2')
         self.option2.addItems(["Potential (V)", "Current (mA)", "DAC Code"])
 
+        self.text_vid = self.findChild(QLineEdit, 'usb_vid')
+        self.text_vid.setText(usb_vid)
+        self.text_pid = self.findChild(QLineEdit, 'usb_pid')
+        self.text_pid.setText(usb_pid)
+
+        self.label_manufacture = self.findChild(QLabel, 'label_manufacture')
+        self.label_product = self.findChild(QLabel, 'label_product')
+        self.label_serial = self.findChild(QLabel, 'label_serial')
+
         self.status_table = 0
         self.status_line = 0
         self.sort_ = 0
+
         # self.frame_x = Frame(self.main_widget)
         # self.frame_x.setStyleSheet("background-color: #181818;")
         # self.frame_x.resize(127, 102)
