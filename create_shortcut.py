6910126b79ddcd79433a762b11b8d5f2d8364c1e
Giang Nguyen Van <giangnv.soict.hust@gmail.com>
2021-01-09 20:45:44 +0700

    Sua file name shortcut

--- create_shortcut.py
+++ create_shortcut.py
@@ -5,5 +5,5 @@ store_dir = r"C:\\"
 repo_dir = os.path.join(store_dir, 'oeps')
 path_file = os.path.join(repo_dir, 'main.py')
 path_icon = os.path.join(repo_dir, 'icon.ico')
-make_shortcut(path_file, name ='OEPS_2', working_dir = repo_dir,
+make_shortcut(path_file, name ='OEPS', working_dir = repo_dir,
                         icon = path_icon, terminal = False)
